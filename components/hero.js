7d4092a9ccb7c7819b4f372c0044392483de6640
Tony de Faria <tonydefaria@iigethr.com>
2020-01-24 12:07:53 +0000

    fit title

--- components/hero.js
+++ components/hero.js
@@ -7,10 +7,10 @@ class Hero extends React.Component {
       <div className="hero">
         <div className="hero-box">
           <div className="hero-row">
-            <h1 className="mono font-xxl magnify lighter text-center white-cl">{name}</h1>
+            <h1 className="mono font-s magnify lighter text-center white-cl">{name}</h1>
           </div>
           <div className="hero-row">
-            <p className="mono font-l lighter text-center gray-500-cl">{description}</p>
+            <p className="mono font-xs lighter text-center gray-500-cl">{description}</p>
           </div>
         </div>
       </div>
